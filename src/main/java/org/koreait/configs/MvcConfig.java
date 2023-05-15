bc891237d6c9cd7dbb79730e91164ad2096200cc
Github <Github>
2023-05-15 12:51:44 +0900

    템플릿 레이아웃 프론트 완료

--- src/main/java/org/koreait/configs/MvcConfig.java
+++ src/main/java/org/koreait/configs/MvcConfig.java
@@ -1,22 +1,29 @@
 package org.koreait.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.koreait.configs.interceptors.SiteConfigInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class MvcConfig implements WebMvcConfigurer {
 
     @Value("${file.upload.path}")
     private String fileUploadPath;
+    
+    //사이트 설정 유지 인터셉터
+    private final SiteConfigInterceptor siteConfigInterceptor;
 
     //메인페이지 설정 틀
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/")
-                .setViewName("main/index"); //(임시 - 바꿀예정)
+                .setViewName("main/index"); //(임시)
     }
 
     //정적 경로 추가 (파일 업로드)
@@ -24,6 +31,13 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:///" + fileUploadPath);
+    }
+
+    //기본 설정 볼수있도록..
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(siteConfigInterceptor)
+                .addPathPatterns("/**");
     }
 }
 
--- src/main/java/org/koreait/configs/SecurityConfig.java
+++ src/main/java/org/koreait/configs/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.koreait.configs;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
